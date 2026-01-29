7edb8d4ba81a5aedbea05d4340c6cd410a24a4a2
Lucas-RibeiroSilva <lucascurso0711@gmail.com>
2026-01-29 15:33:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         const tempo = document.getElementById('tempo').value
 
-        function diminuirtempo(tempo){
+        function diminuirtempo(tempo,tempoInicial){
             timer = setInterval (() => {
             if(tempo <= 0){
                 parartempo(timer);
+            }else if(tempo <= (tempoInicial/2)){
+                cor = "#d3851fff"
+                console.log(tempo),
+                mostrarNumero(tempo,cor),
+                tempo --- 1;
             }else{
                 console.log(tempo),
-                cor = "green"
+                cor = "#10b981"
                 mostrarNumero(tempo,cor),
                 tempo --- 1;
             }
@@ -22,18 +27,19 @@ document.addEventListener('DOMContentLoaded', () => {
         function parartempo(timer){
             setTimeout(() => {
                 clearInterval(timer);
-                console.log("Acabou o tempo")
-                cor = "red"
-                mostrarNumero("Acabou o tempo",cor)
+                console.log("Acabou o tempo!")
+                cor = "#b91010ff"
+                mostrarNumero("Acabou o tempo!",cor)
             }, 0);
         }
 
         function mostrarNumero(recebido,cor){
             document.getElementById("numero").textContent = recebido;
-            document.getElementById("numero").style.color =  cor === "red" ? "red" : "#10b981";
+            document.getElementById("numero").style.color =  cor;
         }
 
-        diminuirtempo(tempo)
+        tempoInicial = tempo;
+        diminuirtempo(tempo,tempoInicial)
     })
 
 })
